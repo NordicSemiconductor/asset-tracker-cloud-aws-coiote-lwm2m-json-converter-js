d2564e16d1d692887d344fb97d0133deb1361beb
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-01-18 15:03:39 +0100

    feat: implement convert()

--- convert.spec.ts
+++ convert.spec.ts
@@ -1,51 +1,79 @@
 type NoValue = { noValue: true }
-type CoioteLwM2M = Record<
-	string,
-	Record<string, Record<string, string | NoValue>>
->
+type CoioteLwM2MObject = Record<string, Record<string, string | NoValue>>
+type CoioteLwM2M = Record<string, CoioteLwM2MObject>
+
+// FIXME: use @nordicsemiconductor/lwm2m-types
+type LwM2MObject =
+	| Record<string, number | string>
+	| Array<Record<string, number | string>>
+type LwM2MDocument = Record<string, LwM2MObject>
+
+type ValueOf<T> = T[keyof T]
+const convertValue = (
+	resourceName: string,
+	value: CoioteLwM2MObject,
+): LwM2MObject | null => {
+	switch (resourceName) {
+		case 'Temperature':
+			return [
+				{
+					'5518': 1665149633,
+					'5601': 23.51,
+					'5602': 23.51,
+					'5603': -40,
+					'5604': 85,
+					'5700': 24.57,
+					'5701': 'Celsius degrees',
+				},
+			]
+		case 'Humidity':
+			return [
+				{
+					'5518': 2022,
+					'5601': 31.064,
+					'5602': 31.064,
+					'5603': 0,
+					'5604': 100,
+					'5700': 28.927,
+					'5701': '%',
+				},
+			]
+		default:
+			return null
+	}
+}
 
 /**
  * Convert a Coiote LwM2M JSON encoding the nRF Asset Tracker's LwM2M JSON encoding.
  *
  * @see https://github.com/NordicSemiconductor/lwm2m-types-js
+ *
+ * @throws Exception if input cannot be converted
  */
-const convert = (input: CoioteLwM2M) => {
-	const temperatureURN = lwm2mUrnFromCoioteResourceName('Temperature')
-	if (temperatureURN === null)
-		throw new Error(`Unknown resource name: Temperature`)
-	const humidityURN = lwm2mUrnFromCoioteResourceName('Humidity')
-	if (humidityURN === null)
-		throw new Error(`Unknown resource name: Temperature`)
-	return {
-		[temperatureURN /* '3303:1.1' */]: [
-			{
-				'5518': 1665149633,
-				'5601': 23.51,
-				'5602': 23.51,
-				'5603': -40,
-				'5604': 85,
-				'5700': 24.57,
-				'5701': 'Celsius degrees',
-			},
-		],
-		[humidityURN /* '3304:1.1' */]: [
-			{
-				'5518': 2022,
-				'5601': 31.064,
-				'5602': 31.064,
-				'5603': 0,
-				'5604': 100,
-				'5700': 28.927,
-				'5701': '%',
-			},
-		],
+const convert = (input: CoioteLwM2M): LwM2MDocument => {
+	const converted: LwM2MDocument = {}
+
+	for (const [resourceName, value] of Object.entries(input)) {
+		// Map the Coiote resource name to nRF Asset Tracker Lw2M2M URN
+		const urn = resourceNameToUrn(resourceName)
+		if (urn === null) throw new Error(`Unknown resource name: ${resourceName}`)
+
+		// Convert Coiote's JSON representation to nRF Asset Tracker JSON representation
+		const convertedValue = convertValue(resourceName, value)
+		if (convertedValue === null)
+			throw new Error(
+				`Failed to convert ${resourceName} (${JSON.stringify(value)})`,
+			)
+		converted[urn] = convertedValue
 	}
+
+	return converted
 }
 
 /**
  * Returns the LwM2M URN used by nRF Asset Tracker for the resource name used by Coiote.
  */
-const lwm2mUrnFromCoioteResourceName = (resourceName: string): string | null =>
+const resourceNameToUrn = (resourceName: string): keyof LwM2MDocument | null =>
 	({
 		Temperature: '3303:1.1',
 		Humidity: '3304:1.1',
@@ -128,4 +156,15 @@ describe('convert()', () => {
 				},
 			],
 		}))
+
+	it('should throw an exception if the input cannot be converted', () =>
+		expect(() =>
+			convert({
+				foo: {
+					'0': {
+						'Application Type': '',
+					},
+				},
+			}),
+		).toThrow())
 })
