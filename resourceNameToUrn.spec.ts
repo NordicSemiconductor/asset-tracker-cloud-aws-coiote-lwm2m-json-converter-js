123313d792fcc98d14c84b6f756f5400e9bfc9ca
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-01-18 15:31:58 +0100

    feat: implement resourceNameToUrn

---
+++ resourceNameToUrn.spec.ts
@@ -0,0 +1,16 @@
+import { resourceNameToUrn } from './resourceNameToUrn'
+
+describe('resourceNameToUrn()', () => {
+	it.each([
+		['Connectivity Monitoring', '4:1.3@1.1'], // ConnectivityMonitoring_4_urn
+		['Humidity', '3304:1.1'], // Humidity_3304_urn
+		// Special case, because of conflict
+		['Location', '6'], // Location_6_urn
+	])(
+		`should convert the resource name "%s" to the URN "%s"`,
+		(resourceName, expectedURN) =>
+			expect(resourceNameToUrn(resourceName)).toEqual(expectedURN),
+	)
+})
+
+// Location Assistance
--- resourceNameToUrn.ts
+++ resourceNameToUrn.ts
@@ -1,13 +1,19 @@
 // FIXME: use @nordicsemiconductor/lwm2m-types
 import type { LwM2MDocument } from './convert'
+import LwM2MSchema from './node_modules/@nordicsemiconductor/lwm2m-types/LwM2MDocument.schema.json'
 
 /**
  * Returns the LwM2M URN used by nRF Asset Tracker for the resource name used by Coiote.
+ *
+ * This assumes that all objects sent by asset_tracker_v2 follow the latest LwM2M definition in the LwM2M object registry.
  */
 export const resourceNameToUrn = (
 	resourceName: string,
-): keyof LwM2MDocument | null =>
-	({
-		Temperature: '3303:1.1',
-		Humidity: '3304:1.1',
-	}[resourceName] ?? null)
+): keyof LwM2MDocument | null => {
+	const match = Object.entries(LwM2MSchema.properties).find(
+		([, { title }]) => title === resourceName,
+	)
+	if (match === undefined) return null
+	const [id] = match
+	return id
+}
