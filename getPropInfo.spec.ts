c316b26ae741bfc92ea90b97ce4bc0c10becaa52
Mauro Lopez <mauro.lopez@nordicsemi.com>
2023-01-19 03:37:42 +0100

    test: should test 'getPropInfo' with edge case

--- getPropInfo.spec.ts
+++ getPropInfo.spec.ts
@@ -3,12 +3,27 @@ import LwM2MSchema from './node_modules/@nordicsemiconductor/lwm2m-types/LwM2MDo
 
 describe("get property info from 'Temperature' resource", () => {
 	const schema: Record<string, any> = LwM2MSchema.properties['3303:1.1']
-	const resourceType = 'array'
+	const resourceType = LwM2MSchema.properties['3303:1.1'].type as any // 'array'
 	it.each([
 		['Min Measured Value', { id: '5601', type: 'number' }],
 		['Application Type', { id: '5750', type: 'string' }],
 		['Measurement Quality Indicator', { id: '6042', type: 'integer' }],
 	])('type from prop (%s) should be (%s) ', (propName, expected) => {
 		expect(getPropInfo(propName, resourceType, schema)).toMatchObject(expected)
+	})
+
+	it('should return id and data type from "rsrp-Result" ', () => {
+		const schema: Record<string, any> = LwM2MSchema.properties['10256'] // 'array'
+		const resourceType = LwM2MSchema.properties['10256'].type as any
+		const correctValue = 'rsrp-Result '
+		const incorrectValue = 'rsrp-Result'
+
+		// just one white space between both values
+
+		expect(getPropInfo(correctValue, resourceType, schema)).toMatchObject({
+			id: '3',
+			type: 'integer',
+		})
+		expect(getPropInfo(incorrectValue, resourceType, schema)).toBe(undefined)
 	})
 })
